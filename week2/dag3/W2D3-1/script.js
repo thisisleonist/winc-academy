f01ae0725f1d3a9910ecafa4f7f31513ea10f7f4
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-03-25 08:25:06 +0100

    W2D3-1 Update

---
+++ week2/dag3/W2D3-1/script.js
@@ -0,0 +1,39 @@
+// function sum(a, b){
+//     return a + b;
+// }
+
+// const sum2 = (a, b) => a + b;
+
+// function isPositive(number){
+//     return number >= 0;
+// }
+
+// const isPositive2 = number => number >= 0;
+
+// function randomNumber(){
+//     return  Math.random;
+// }
+
+// const randomNumber2 = () => Math.random;
+
+// document.addEventListener('click', function(){
+//     console.log('Click');
+// });
+
+// document.addEventListener('click', () => console.log('Click'));
+
+const ikRockArrowFunctions = () => console.log('Joe, ik rock de arrow functions!');
+
+ikRockArrowFunctions();
+
+const fivePlusSeven = () => 5 + 7;
+  
+console.log( fivePlusSeven() );
+
+() => 1 + 2;
+
+const myFunction = (a, b) => a + b;
+
+const addFive = a => a + 5;
+
+const createObject = () => ({greeting: "hoi"});
