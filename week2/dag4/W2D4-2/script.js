d886fb7c83b47032bdc917ddf75356faf2909418
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-03-26 13:24:18 +0100

    W2D4-2 Update

--- week2/dag4/W2D4-2/script.js
+++ week2/dag4/W2D4-2/script.js
@@ -8,3 +8,69 @@
  * 
  */
 
+const bigFiveButtons = document.getElementsByClassName('big-five-button');
+
+const setEventListeners = () => {
+    Array.from(bigFiveButtons).forEach(item => {
+        item.addEventListener('click',() => {
+            addBigFive(item.innerHTML);
+        });
+    });
+};
+
+setEventListeners();
+
+const spottedAnimalsList = document.getElementById('spotted-animals-list');
+const spottedAnimals = document.getElementsByClassName('spotted-animals-list-item');
+
+const addBigFive = animalName => {
+
+    const animalSpottedState = Array.from(spottedAnimals).some((item) => {
+        return item.innerHTML === animalName;
+    });
+
+    if(animalSpottedState===false){
+        const listItem = document.createElement("li");
+        listItem.classList.add("spotted-animals-list-item");
+        listItem.innerHTML = animalName;
+        spottedAnimalsList.appendChild(listItem);
+    }
+
+};
+
+const removeFirstAnimal = document.getElementById('remove-first-item-button');
+
+const setRemoveFirstAnimalEvent = () => {
+    removeFirstAnimal.addEventListener('click', event => {
+        removeSpottedAnimal(event);
+    });
+};
+
+setRemoveFirstAnimalEvent();
+
+const removeSpottedAnimal = event => {
+
+    const animalSpottedState = Array.from(spottedAnimals).find((item) => {
+        return item;
+    });
+    if(animalSpottedState!==undefined){
+        animalSpottedState.remove();
+    }
+    
+};
+
+const removeAllAnimals = document.getElementById('remove-all-button');
+
+const setRemoveAllAnimalsEvent = () => {
+    removeAllAnimals.addEventListener('click', event => {
+        removeAllSpottedAnimals(event);
+    });
+};
+
+setRemoveAllAnimalsEvent();
+
+const removeAllSpottedAnimals = event => {
+    Array.from(spottedAnimals).forEach((item) => {
+        item.remove();
+    });
+};
