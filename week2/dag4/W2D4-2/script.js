24a2cf9d36bc8d0defa64368dd3ec39866692b26
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-03-26 12:45:33 +0100

    Opdracht W2D4-2

---
+++ week2/dag4/W2D4-2/script.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * Basis voor opbouwen list <ul> van items <li>
+ * const listElement = document.createElement("li")
+ * listItem.classList.add("list-item-class");
+ * const list = document.querySelector("ul");
+ * list.appendChild(listItem);
+ * 
+ */
+
