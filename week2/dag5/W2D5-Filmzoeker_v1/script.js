6927de2408fb37094d7746b6ef486467f943df64
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-03-26 18:49:25 +0100

    W2D5-Filmzoeker Updates

--- week2/dag5/W2D5-Filmzoeker_v1/script.js
+++ week2/dag5/W2D5-Filmzoeker_v1/script.js
@@ -10,12 +10,22 @@ const buildMovieItem = metadata => {
     //console.log(metadata);
 
     const movieItem = document.createElement("div");
-    movieItem.setAttribute('class', 'movie');
+    if(metadata.Type==='movie'){
+        movieItem.setAttribute('class', 'movie highlight-movie');
+    }else if(metadata.Type==='series'){
+        movieItem.setAttribute('class', 'movie highlight-series');
+    }
     movieListing.appendChild(movieItem);
+
+    const movieLinkTo = document.createElement("a");
+    movieLinkTo.setAttribute('href', 'https://www.imdb.com/title/'+metadata.imdbID+'/');
+    movieLinkTo.setAttribute('rel', 'nofollow noopener');
+    movieLinkTo.setAttribute('target', '_blank');
+    movieItem.appendChild(movieLinkTo);
 
     const movieImage = document.createElement("img");
     movieImage.setAttribute('src', metadata.Poster);
-    movieItem.appendChild(movieImage);
+    movieLinkTo.appendChild(movieImage);
 
     const movieYear = document.createElement("div");
     movieYear.setAttribute('class', 'year');
@@ -40,5 +50,76 @@ const buildMovieItem = metadata => {
 
 buildMovieItems();
 
+const filterButtons = document.querySelectorAll('#filter-listing label');
 
+const setFilterButtonEvents = () => {
+    Array.from(filterButtons).forEach(button => {
+        button.addEventListener('click', event => {
+            applyFilter(event.toElement.htmlFor);
+        });
+    });
+};
 
+setFilterButtonEvents();
+
+const applyFilter = filter => {
+    switch(filter){
+        case 'latest':
+            const latest = Array.from(movies.Movies).filter(movie => {
+                return movie.Year >= 2014;
+            });
+            resetMovieListing();
+            latest.forEach(metadata => buildMovieItem(metadata));
+            break;
+        case 'avengers':
+            const avengers = Array.from(movies.Movies).filter(movie => {
+                return movie.Title.includes('Avengers');
+            });
+            resetMovieListing();
+            avengers.forEach(metadata => buildMovieItem(metadata));
+            break;
+        case 'xmen':
+            const xmen = Array.from(movies.Movies).filter(movie => {
+                return movie.Title.includes('X-Men');
+            });
+            resetMovieListing();
+            xmen.forEach(metadata => buildMovieItem(metadata));
+            break;
+        case 'princess':
+            const princess = Array.from(movies.Movies).filter(movie => {
+                return movie.Title.includes('Princess');
+            });
+            resetMovieListing();
+            princess.forEach(metadata => buildMovieItem(metadata));
+            break;
+        case 'batman':
+            const batman = Array.from(movies.Movies).filter(movie => {
+                return movie.Title.includes('Batman');
+            });
+            resetMovieListing();
+            batman.forEach(metadata => buildMovieItem(metadata));
+            break;
+    }
+};
+
+const resetMovieListing = () => movieListing.innerHTML = '';
+
+const searchField = document.querySelector('#search-form input');
+
+const setSearchFieldEvent = () => {
+    searchField.addEventListener('keypress', event => {
+        if (event.keyCode === 13) {
+            performSearch(searchField.value);
+        }
+    });
+};
+
+setSearchFieldEvent();
+
+const performSearch = (searchString) => {
+    const searchResult = Array.from(movies.Movies).filter(movie => {
+        return (movie.Title).toLowerCase().includes(searchString.toLowerCase());
+    });
+    resetMovieListing();
+    searchResult.forEach(metadata => buildMovieItem(metadata));
+};
