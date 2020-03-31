608351f68e0573c9b7ce094252d7a701778d7ac8
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-03-31 13:11:17 +0200

    Extra opdracht

--- week3/dag2/W3D2-3/extra.js
+++ week3/dag2/W3D2-3/extra.js
@@ -5,7 +5,34 @@ returns a Promise that tests if the value is less than or greater
 than the value 10. Log the result to the console.
 */
 
-const testNum = () => {};
+const log = log => console.log(log);
+
+const testNum = (number) => {
+    const max = 10;
+    return new Promise((resolve, reject) => {
+        if(number < max){
+            resolve(`Nope, ${number} is less than ${max}`);
+        }else if(number > max){
+            resolve(`Yes, ${number} is more than ${max}`);
+        }
+        reject(`Hmmm, ${number} is neither`);
+    });
+};
+
+testNum(9)
+    .then(result => log(result))
+    .catch(result => log(result))
+;
+
+testNum(10)
+    .then(result => log(result))
+    .catch(result => log(result))
+;
+
+testNum(11)
+    .then(result => log(result))
+    .catch(result => log(result))
+;
 
 /*
 Exercise 2:
@@ -17,11 +44,45 @@ it should throw an error.
 Then call these functions by *chaining* the promises
 */
 
-const makeAllCaps = () => {};  
-const sortWords = () => {};
-
 const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
 const complicatedArray = ['cucumber', 44, true];
 
 /* call both functions, chain them together and
 log the result to the console */
+
+const makeAllCaps = (array) => {
+    return new Promise((resolve, reject) => {
+        const upper = [];
+        array.forEach((value, index) => {
+            if(typeof(value)!=='string'){
+                reject(`Item ${index} ('${value}') is not a string`);
+            }else{
+                upper.push(value.toUpperCase());
+            }
+        });
+        resolve(upper);
+    });
+};
+
+// makeAllCaps(arrayOfWords)
+//     .then(result => console.log(result))
+//     .catch(err => log(err));
+
+// makeAllCaps(complicatedArray)
+//     .then(result => console.log(result))
+//     .catch(err => log(err));
+
+const sortWords = (array) => {
+    return new Promise((resolve, reject) => {
+        resolve(array.sort());
+    });
+};
+
+makeAllCaps(arrayOfWords)
+    .then(result => {
+        log(result);
+        sortWords(result)
+        .then(result => log(result))
+        .catch(err => log(err));
+    })
+.catch(err => log(err));
