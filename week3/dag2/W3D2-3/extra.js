1bf8e389d7e3b1a1ba33595c13d4bb111675a2f3
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-03-31 13:23:25 +0200

    Updates from terminal ;)

--- week3/dag2/W3D2-3/extra.js
+++ week3/dag2/W3D2-3/extra.js
@@ -10,28 +10,27 @@ const log = log => console.log(log);
 const testNum = (number) => {
     const max = 10;
     return new Promise((resolve, reject) => {
-        if(number < max){
-            resolve(`Nope, ${number} is less than ${max}`);
+        if(number <= max){
+            reject(`Nope, ${number} is equal to or less than ${max}`);
         }else if(number > max){
             resolve(`Yes, ${number} is more than ${max}`);
         }
-        reject(`Hmmm, ${number} is neither`);
     });
 };
 
 testNum(9)
     .then(result => log(result))
-    .catch(result => log(result))
+    .catch(error => log(error))
 ;
 
 testNum(10)
     .then(result => log(result))
-    .catch(result => log(result))
+    .catch(error => log(error))
 ;
 
 testNum(11)
     .then(result => log(result))
-    .catch(result => log(result))
+    .catch(error => log(error))
 ;
 
 /*
@@ -50,19 +49,21 @@ const complicatedArray = ['cucumber', 44, true];
 /* call both functions, chain them together and
 log the result to the console */
 
-const makeAllCaps = (array) => {
-    return new Promise((resolve, reject) => {
-        const upper = [];
-        array.forEach((value, index) => {
-            if(typeof(value)!=='string'){
-                reject(`Item ${index} ('${value}') is not a string`);
-            }else{
-                upper.push(value.toUpperCase());
-            }
-        });
-        resolve(upper);
-    });
-};
+// Mijn versie:
+
+// const makeAllCaps = (array) => {
+//     return new Promise((resolve, reject) => {
+//         const upper = [];
+//         array.forEach((value, index) => {
+//             if(typeof(value)!=='string'){
+//                 reject(`Item ${index} ('${value}') is not a string`);
+//             }else{
+//                 upper.push(value.toUpperCase());
+//             }
+//         });
+//         resolve(upper);
+//     });
+// };
 
 // makeAllCaps(arrayOfWords)
 //     .then(result => console.log(result))
@@ -72,17 +73,69 @@ const makeAllCaps = (array) => {
 //     .then(result => console.log(result))
 //     .catch(err => log(err));
 
-const sortWords = (array) => {
-    return new Promise((resolve, reject) => {
-        resolve(array.sort());
-    });
-};
+// const sortWords = (array) => {
+//     return new Promise((resolve, reject) => {
+//         resolve(array.sort());
+//     });
+// };
 
-makeAllCaps(arrayOfWords)
-    .then(result => {
-        log(result);
-        sortWords(result)
-        .then(result => log(result))
-        .catch(err => log(err));
-    })
-.catch(err => log(err));
+// makeAllCaps(arrayOfWords)
+//     .then(result => {
+//         log(result);
+//         sortWords(result)
+//         .then(result => log(result))
+//         .catch(err => log(err));
+//     })
+// .catch(err => log(err));
+
+// De oplossing:
+
+const makeAllCaps = words => {
+    return new Promise((resolve, reject) => {
+      if (
+        words.every(word => {
+          return typeof word === 'string';
+        })
+      ) {
+        resolve(
+          sortWords(
+            words.map(word => {
+              return word.toUpperCase();
+            })
+          )
+        );
+      } else {
+        reject('Not a string!');
+      }
+    });
+  };
+  
+  const sortWords = words => {
+    return new Promise((resolve, reject) => {
+      if (words) {
+        resolve(words.sort());
+      } else {
+        reject('strings only!');
+      }
+    });
+  };
+  
+  const theseAreWords = ['promise', 'practice', 'break'];
+  
+  makeAllCaps(theseAreWords)
+    .then(sortWords(theseAreWords))
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+  
+  const theseAreNotWords = [1, 'hello', 9];
+  
+  makeAllCaps(theseAreNotWords)
+    .then(sortWords(theseAreNotWords))
+    .then(result => console.log(result))
+    .catch(error => console.log(error));
+
+// Voor het checken van typeof is de oplossing cleaner
+// Merk wel in het voorbeeld dat twee keer sortWords() wordt gebruikt
+// In de functie makeAllCaps() en dan weer in makeAllCaps.then()
+
+// Link: https://repl.it/@JasonHughes/Promise-Practice
