7478d587f23f72e2ab13ae0050fe8d55ccaebd15
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-03-31 13:26:28 +0200

    Updates from terminal ;)

---
+++ week3/dag2/W3D2/example.js
@@ -0,0 +1,16 @@
+// Voorbeeld uit YouTube uitleg over callback & promises
+
+const log = msg => console.log(msg);
+
+const p = new Promise ( (resolve, reject) => {
+    setTimeout(() => {
+        resolve('OK');
+    }, 500);
+    //reject('Error');
+});
+
+p.then(() => {
+    log('THEN');
+});
+
+log(p);
--- week3/dag2/W3D2/script.js
+++ week3/dag2/W3D2/script.js
@@ -1,16 +1,34 @@
-// Voorbeeld uit YouTube uitleg over callback & promises
-
 const log = msg => console.log(msg);
+const logposts = () => {
+    log(posts);
+};
+const posts = [];
 
-const p = new Promise ( (resolve, reject) => {
-    setTimeout(() => {
-        resolve('OK');
-    }, 500);
-    //reject('Error');
-});
+function createPost(post) {
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            posts.push(post);
+            const error = false;
+            if(!error){
+                resolve();
+            } else {
+                reject('Error');
+            }
+        }, 2000);
+    });
+}
 
-p.then(() => {
-    log('THEN');
-});
+createPost(
+    {title: 'test', body: 'test'}
+).then(
+    logposts
+).catch(
+    err => log(err)
+);
 
-log(p);
+const promise1 = Promise.resolve('Hello World!');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) => setTimeout(resolve, 2000, 'Goodbye'));
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+Promise.all([promise1, promise2, promise3, promise4]).then(values => log(values));
