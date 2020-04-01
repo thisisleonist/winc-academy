0079c6728de08d97df5d9428d524f511d52051ad
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-04-01 10:10:20 +0200

    W3D3-1 Assignment done

--- week3/dag3/W3D3-1/script.js
+++ week3/dag3/W3D3-1/script.js
@@ -1,2 +1,25 @@
+// 1
+
 const log = log => console.log(log);
 
+const sum = (...numbers) => {
+    return numbers.reduce((prev, current) => {
+        return prev + current;
+    });
+};
+
+log(
+    sum(1, 2, 3, 4, 5)
+);
+
+// 2
+
+const sum2 = (numbers) => {
+    return numbers.reduce((prev, current) => {
+        return prev + current;
+    });
+};
+
+log(
+    sum(...[1, 2, 3, 4, 5])
+);
