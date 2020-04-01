f23822d826f5835b4a28ad0471047906f6ab3c87
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-04-01 17:15:13 +0200

    W3D3-4 Update

--- week3/dag3/W3D3-4/api-client.js
+++ week3/dag3/W3D3-4/api-client.js
@@ -1,7 +1,6 @@
 // https://www.themoviedb.org/settings/api
 // Changed JSLint to ES8
 
-const API_URL = 'https://api.themoviedb.org/3/genre/movie/list';
 const API_KEY = '79c1f23d5822955801447c6490744b56';
 
 const log = log => console.log(log);
@@ -10,6 +9,17 @@ const getData = async apiUrl => {
     try{
         const url = `${apiUrl}?api_key=${API_KEY}`;
         const res = await fetch(url,{method: 'GET'});
+        const data = await res.json();
+        //log(data);
+        return data;
+    } catch(error) {
+        //log(error);
+    }
+};
+
+const getMovieData = async apiUrl => {
+    try{
+        const res = await fetch(`${apiUrl}&api_key=${API_KEY}`,{method: 'GET'});
         const data = await res.json();
         //log(data);
         return data;
--- week3/dag3/W3D3-4/script.js
+++ week3/dag3/W3D3-4/script.js
@@ -1,5 +1,77 @@
-const handleData = async url => {
-    return data = await getData(url);
+const MOVIE_GENRES_URL = 'https://api.themoviedb.org/3/genre/movie/list';
+
+const body = document.body;
+
+// const getMovieGenres = url => {
+//     getData(url).then(data => buildMovieGenres(data.genres));
+// };
+
+// const buildMovieGenres = items => {
+//     const header = document.createElement('h1');
+//     header.innerHTML = 'Movie Genres with ID';
+//     body.appendChild(header);
+//     const listing = document.createElement('ul');
+//     body.appendChild(listing);
+//     items.forEach(item => {
+//         let listitem = document.createElement('li');
+//         listitem.innerHTML = `${item.name} (id: ${item.id})`;
+//         listing.appendChild(listitem);
+//         listitem = '';
+//     });
+// };
+
+// getMovieGenres(MOVIE_GENRES_URL);
+
+const getOneMovie = query => {
+    getMovieData(`https://api.themoviedb.org/3/search/movie?query=${query}`)
+    .then(data => {
+        metadata = data.results[0];
+        id = metadata.id;
+        getMovieData(`https://api.themoviedb.org/3/movie/${id}/external_ids?`)
+        .then(data => {
+            imdb_id = data.imdb_id;
+            getMovieData(`https://api.themoviedb.org/3/find/${imdb_id}?external_source=imdb_id`)
+            .then(data => {
+                console.log(data.movie_results[0]);
+                buildOneMovie(data.movie_results[0]);
+            });
+        });
+    });
 };
-  
-getData(API_URL).then(data => console.log(data.genres[0]));
+
+const buildOneMovie = item => {
+    const header = document.createElement('h1');
+    header.innerHTML = `One Movie: ${item.title} (id: ${item.id})`;
+    body.appendChild(header);
+    const listing = document.createElement('ul');
+    body.appendChild(listing);
+    const keys = [];
+    let listitem = document.createElement('li');
+    listitem.innerHTML = `${item.title} (id: ${item.id})`;
+    listing.appendChild(listitem);
+};
+
+getOneMovie('The Matrix');
+
+// const getSingleMovie = (url,) => {
+//     getMovieData(`${url}?query=The,Lion,King&year=2019`).then(data => {
+//         buildSingleMovie(data.results[0]);
+//     });
+// };
+
+// const buildSingleMovie = item => {
+//     const header = document.createElement('h1');
+//     header.innerHTML = ` ${item.title} ${item.year}`;
+//     body.appendChild(header);
+//     const listing = document.createElement('ul');
+//     body.appendChild(listing);
+//     let listitem = document.createElement('li');
+//     listitem.innerHTML = `${item.title} (id: ${item.id})`;
+//     listing.appendChild(listitem);
+// };
+
+// getSingleMovie();
+
+
+
+//https://api.themoviedb.org/3/search/movie?api_key=79c1f23d5822955801447c6490744b56&
