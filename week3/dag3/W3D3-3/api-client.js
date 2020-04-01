f8f4dd9d22408b880d65ea40afdd7c95444fd027
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-04-01 14:38:03 +0200

    Update

--- week3/dag3/W3D3-3/api-client.js
+++ week3/dag3/W3D3-3/api-client.js
@@ -1,8 +1,8 @@
 // https://www.themoviedb.org/settings/api
 // Changed JSLint to ES8
 
-const API_KEY = '79c1f23d5822955801447c6490744b56';
 const API_URL = 'https://api.themoviedb.org/3/genre/movie/list';
+const API_KEY = '79c1f23d5822955801447c6490744b56';
 
 const log = log => console.log(log);
 
