ee93117ac0e87e9196513564adde043e753ef32f
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-04-02 07:45:15 +0200

    W3D3-4 Assignment update cleanup code

--- week3/dag3/W3D3-4/api-client.js
+++ week3/dag3/W3D3-4/api-client.js
@@ -2,28 +2,32 @@
 // Changed JSLint to ES8
 
 const API_KEY = '79c1f23d5822955801447c6490744b56';
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 
 const log = log => console.log(log);
 
 const getData = async apiUrl => {
-    try{
-        const url = `${apiUrl}?api_key=${API_KEY}`;
-        const res = await fetch(url,{method: 'GET'});
+    try {
+        const completeUrl = `${apiUrl}?api_key=${API_KEY}`;
+        const res = await fetch(completeUrl,{
+            method: 'GET'
+        });
         const data = await res.json();
-        //log(data);
         return data;
     } catch(error) {
-        //log(error);
+        log(error);
     }
 };
 
 const getMovieData = async apiUrl => {
-    try{
-        const res = await fetch(`${apiUrl}&api_key=${API_KEY}`,{method: 'GET'});
+    try {
+        const completeUrl = `${apiUrl}&api_key=${API_KEY}`;
+        const res = await fetch(completeUrl,{
+            method: 'GET'
+        });
         const data = await res.json();
-        //log(data);
         return data;
     } catch(error) {
-        //log(error);
+        log(error);
     }
 };
--- week3/dag3/W3D3-4/script.js
+++ week3/dag3/W3D3-4/script.js
@@ -1,22 +1,15 @@
-const MOVIE_GENRES_URL = 'https://api.themoviedb.org/3/genre/movie/list';
-const MOVIE_TOP_RATED_URL = 'https://api.themoviedb.org/3/movie/top_rated';
-const MOVIE_TOP_RATED_GENRE_ACTION_URL =
-    'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&with_genres=28';
-const MOVIES_FROM_1975_URL =
-    'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&page=1&year=1975';
-
-const body = document.body;
-
 const getMovieGenres = url => {
-    getData(url).then(data => displayMovieGenres(data.genres));
+    getData(url)
+    .then(data => displayMovieGenres(data.genres))
+    .catch(error => console.log(error));
 };
 
 const displayMovieGenres = items => {
-    const header = document.createElement('h1');
-    header.innerHTML = 'Movie Genres with ID';
-    body.appendChild(header);
-    const listing = document.createElement('ul');
-    body.appendChild(listing);
+    const header = document.createElement('h2');
+    header.innerHTML = 'TheMovieDB Genres';
+    document.body.appendChild(header);
+    const listing = document.createElement('ol');
+    document.body.appendChild(listing);
     items.forEach(item => {
         let listitem = document.createElement('li');
         listitem.innerHTML = `${item.name} (id: ${item.id})`;
@@ -25,40 +18,45 @@ const displayMovieGenres = items => {
     });
 };
 
-const getASingleMovie = (query, year, isfav) => {
+const getSingleMovie = (query, year, isAllTimeFavorite) => {
     const url = `https://api.themoviedb.org/3/search/movie?query=${query}&year=${year}`;
     getMovieData(url)
     .then(data => {
         if(data.results.length!==0){
             const metadata = data.results[0];
-            const id = metadata.id;
-            getMovieData(`https://api.themoviedb.org/3/movie/${id}/external_ids?`)
-            .then(data => {
-                const imdb_id = data.imdb_id;
-                getMovieData(`https://api.themoviedb.org/3/find/${imdb_id}?external_source=imdb_id`)
-                .then(data => {
-                    if(isfav===undefined){
-                        isfav = false;
-                    }
-                    if(isfav===true){
-                        displayOneMovie(data.movie_results[0],'Favorite Movie of All Time (from IMDB)');
-                    }else{
-                        displayOneMovie(data.movie_results[0],'Single Movie in Postman (from IMDB)');
-                    }
-                });
-            });
+            getMovieID(metadata.id, isAllTimeFavorite);
         }else{
             displayNotFound(query, year);
         }
-    });
+    })
+    .catch(error => console.log(error));
 };
 
-const displayOneMovie = (item, prefix) => {
-    const header = document.createElement('h1');
+const getMovieID = (id, isAllTimeFavorite) => {
+    getMovieData(`https://api.themoviedb.org/3/movie/${id}/external_ids?`)
+    .then(data => {
+        getMovieDetails(data.imdb_id, isAllTimeFavorite);
+    })
+    .catch(error => console.log(error));
+};
+
+const getMovieDetails = (id, isAllTimeFavorite) => {
+    getMovieData(`https://api.themoviedb.org/3/find/${id}?external_source=imdb_id`)
+    .then(data => {
+        if(isAllTimeFavorite===undefined || isAllTimeFavorite===false){
+            return displayMovieDetails(data.movie_results[0],'Single Movie');
+        }
+        return displayMovieDetails(data.movie_results[0],'Favorite Movie of All Time');
+    })
+    .catch(error => console.log(error));
+};
+
+const displayMovieDetails = (item, prefix) => {
+    const header = document.createElement('h2');
     header.innerHTML = `${prefix}: ${item.title}`;
-    body.appendChild(header);
+    document.body.appendChild(header);
     const listing = document.createElement('ul');
-    body.appendChild(listing);
+    document.body.appendChild(listing);
     const listitem1 = document.createElement('li');
     listitem1.innerHTML = `ID: ${item.id}`;
     listing.appendChild(listitem1);
@@ -71,26 +69,28 @@ const displayOneMovie = (item, prefix) => {
 };
 
 const displayNotFound = (query, year) => {
-    const header = document.createElement('h1');
+    const header = document.createElement('h2');
     header.innerHTML = `Single Movie in Postman: ${query}`;
-    body.appendChild(header);
+    document.body.appendChild(header);
     const listing = document.createElement('ul');
-    body.appendChild(listing);
+    document.body.appendChild(listing);
     const listitem1 = document.createElement('li');
     listitem1.innerHTML = `A movie with qeury “${query}” in year ${year} could not be found`;
     listing.appendChild(listitem1);
 };
 
 const getTopRated = url => {
-    getData(url).then(data => displayTopRated(data));
+    getData(url)
+    .then(data => displayTopRated(data))
+    .catch(error => console.log(error));
 };
 
 const displayTopRated = items => {
-    const header = document.createElement('h1');
+    const header = document.createElement('h2');
     header.innerHTML = 'Top Rated Movies with ID';
-    body.appendChild(header);
+    document.body.appendChild(header);
     const listing = document.createElement('ul');
-    body.appendChild(listing);
+    document.body.appendChild(listing);
     items.results.forEach(item => {
         let listitem = document.createElement('li');
         listitem.innerHTML = `${item.title} (id: ${item.id})`;
@@ -100,15 +100,17 @@ const displayTopRated = items => {
 };
 
 const getTopRatedGenreAction = url => {
-    getMovieData(url).then(data => displayTopRatedGenreAction(data));
+    getMovieData(url)
+    .then(data => displayTopRatedGenreAction(data))
+    .catch(error => console.log(error));
 };
 
 const displayTopRatedGenreAction = items => {
-    const header = document.createElement('h1');
+    const header = document.createElement('h2');
     header.innerHTML = 'Top Rated Movies in Genre Action with ID';
-    body.appendChild(header);
+    document.body.appendChild(header);
     const listing = document.createElement('ul');
-    body.appendChild(listing);
+    document.body.appendChild(listing);
     items.results.forEach(item => {
         let listitem = document.createElement('li');
         listitem.innerHTML = `${item.title} (id: ${item.id})`;
@@ -118,15 +120,17 @@ const displayTopRatedGenreAction = items => {
 };
 
 const getMoviesFrom1975 = url => {
-    getMovieData(url).then(data => displayMoviesFrom1975(data));
+    getMovieData(url)
+    .then(data => displayMoviesFrom1975(data))
+    .catch(error => console.log(error));
 };
 
 const displayMoviesFrom1975 = items => {
-    const header = document.createElement('h1');
+    const header = document.createElement('h2');
     header.innerHTML = 'Movies from 1975 with ID';
-    body.appendChild(header);
+    document.body.appendChild(header);
     const listing = document.createElement('ul');
-    body.appendChild(listing);
+    document.body.appendChild(listing);
     items.results.forEach(item => {
         let listitem = document.createElement('li');
         listitem.innerHTML = `${item.title} (id: ${item.id})`;
@@ -135,11 +139,13 @@ const displayMoviesFrom1975 = items => {
     });
 };
 
-getMovieGenres(MOVIE_GENRES_URL);
-getASingleMovie('The Lion King', 2019);
-getASingleMovie('The Joker', 2019); // not found
-getASingleMovie('Toy Story 4', 2019);
-getASingleMovie('The Matrix', 1999, true);
-getTopRated(MOVIE_TOP_RATED_URL);
-getTopRatedGenreAction(MOVIE_TOP_RATED_GENRE_ACTION_URL);
-getMoviesFrom1975(MOVIES_FROM_1975_URL);
+getMovieGenres(API_BASE_URL + '/genre/movie/list');
+getSingleMovie('The Lion King', 2019);
+getSingleMovie('Toy Story 4', 2019);
+ // not found!
+getSingleMovie('The Joker', 2019);
+// all time favorite
+getSingleMovie('The Matrix', 1999, true);
+getTopRated(API_BASE_URL + '/movie/top_rated');
+getTopRatedGenreAction(API_BASE_URL + '/discover/movie?sort_by=popularity.desc&page=1&with_genres=28');
+getMoviesFrom1975(API_BASE_URL + '/discover/movie?sort_by=popularity.desc&page=1&year=1975');
