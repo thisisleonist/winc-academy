70d0c73200f1e80ee7b53d3d329bf9333920933f
Leon Rijsdam <leon.rijsdam@outlook.com>
2020-04-16 10:06:39 +0200

    Update VSCode setup

--- week5/dag3/W5D3-1/my-groceries-app/.eslintrc.js
+++ week5/dag3/W5D3-1/my-groceries-app/.eslintrc.js
@@ -21,5 +21,15 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        "react/jsx-filename-extension": [
+            2,
+            {
+              "extensions": [".js", ".jsx"]
+            }
+          ],
+          "react/forbid-prop-types": [0],
+          "react/require-default-props": [0],
+          "global-require": [0],
+          "no-underscore-dangle": [0]
     }
 };
